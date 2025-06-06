3fc36da0afe43a2875c099e3b836fb4863fa8f19
AlanZalazar <alan_zz_@hotmail.com>
2025-06-06 04:42:41 -0300

    subiendo practica 7

--- practica 7/main.js
+++ practica 7/main.js
@@ -15,22 +15,55 @@ const productos = [
   { nombre: "Tablet", precio: 500 },
 ];
 
-//Recorrer y mostrar productos (forEach)
+console.log("1️⃣ Recorrer y mostrar productos y precios (forEach)");
 productos.forEach((e) => {
   console.log(`${e.nombre}: $${e.precio}.00`);
 });
 
-//Crear un array de nombres de productos y verificar disponibilidad (map + includes)
+console.log(
+  "2️⃣ Crear un array de nombres de productos y verificar disponibilidad (map + includes)"
+);
+
 let nombreProductos = productos.map((e) => e.nombre);
 console.log(nombreProductos);
 
 function estaDisponible(nombre) {
-  if (nombreProductos.includes(nombre.toLowerCase())) {
+  let productosLower = nombreProductos.map((e) => e.toLowerCase());
+  if (productosLower.includes(nombre.toLowerCase())) {
     console.log(`El producto ${nombre} se encuentra disponible`);
   } else {
     console.log(`El producto ${nombre} no está disponible`);
   }
 }
-
-estaDisponible("Monitor");
+estaDisponible("monitor");
 estaDisponible("reflector");
+
+console.log("3️⃣ Aplicar un descuento a los productos (map)");
+
+let productosDescuento = productos.map((e) => {
+  return {
+    nombre: e.nombre,
+    precio: e.precio * 0.9,
+  };
+});
+console.log(productosDescuento);
+
+console.log("4️⃣ Filtrar productos por precio (filter)");
+
+let menorA100 = productos.filter((e) => e.precio < 100);
+console.log(menorA100);
+
+console.log("5️⃣ Obtener los primeros productos (slice)");
+
+let dosPrimeros = productos.slice(0, 2);
+console.log(dosPrimeros);
+
+console.log("6️⃣ Ordenar productos por precio (sort)");
+let productosOrdenadosPorPrecio = [...productos].sort(
+  (a, b) => a.precio - b.precio
+);
+console.log(productosOrdenadosPorPrecio);
+
+console.log("7️⃣ Invertir el orden de los productos (reverse)");
+let productosInvertidos = [...productos].reverse();
+console.log(productosInvertidos);
